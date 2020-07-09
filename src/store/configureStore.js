bdccb80fd0c10224c5ad077a14bdb1e744a78948
douglasryu <douglasryu@hotmail.com>
2020-07-09 12:06:44 -0400

    Update connections

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 import rootReducer from "../reducers/rootReducer";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -7,7 +8,7 @@ const configureStore = (initialState = {}) => {
     return createStore(
         rootReducer,
         initialState,
-        composeEnhancers(applyMiddleware()),
+        composeEnhancers(applyMiddleware(thunk)),
     );
 };
 
