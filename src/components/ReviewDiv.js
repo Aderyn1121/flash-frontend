0e044c19aa5b9eab264c491fdacf1d8c3be57f8a
Addy <zaristus@gmail.com>
2020-07-09 14:12:15 -0700

    loading reviews functions

--- src/components/ReviewDiv.js
+++ src/components/ReviewDiv.js
@@ -3,23 +3,45 @@ import { connect } from "react-redux";
 import { fetchReviews } from "../actions/reviewActions";
 
 const ReviewDiv = (props) => {
+  let reviews = {};
+
   let newReviews = [];
 
   if (props.reviews !== undefined) {
-    let reviews = props.reviews;
-    for (let review in reviews) {
-      newReviews.push(review);
-      console.log(newReviews);
-    }
+    reviews = props.reviews;
+
+    Object.values(reviews).forEach((item) => {
+      newReviews.push(item)
+    })
+    
+    
+
   }
+
+  console.log(newReviews)
+
+  if(newReviews.length === 0) return(
+    <div className="products__review-container">
+      <form className="products__review-form">
+      {/* onSubmit = this.postReview function */}
+      <textarea
+        className="products__review-field"
+        type="text"
+        placeholder="Submit a review"
+      ></textarea>
+      <button className="products__review-submit">Submit</button>
+    </form>
+  </div>
+  )
+
 
   return (
     <div className="products__review-container">
       {newReviews.map((review, i) => {
         return (
           <div key={i} className="products__review-block">
-            <div className="products__review-author">Anonymous</div>
-            <div className="products__review-content">{review.content}</div>
+            <div className="products__review-author">{`${review.firstName} ${review.lastName}`}</div>
+            <div className="products__review-content">{review.reviewBody}</div>
           </div>
         );
       })}
