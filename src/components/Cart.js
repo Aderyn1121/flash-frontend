93704390930a4e720369154812de3186ed5cc3b2
douglasryu <douglasryu@hotmail.com>
2020-07-10 19:22:54 -0400

    Update cart css

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -21,23 +21,24 @@ const Cart = (props) => {
         <>
             <Navigation />
             <CategoryBar />
-            <main className="cart__main">
+            <div className="cart__container">
+                <div className="cart__header">Cart</div>
                 {targetProducts.map((product, i) => {
                     total += product.price;
                     return (
-                        <div className="cart__item">
-                            <div className="cart__item-img"><img src={product.imgUrl} alt={product.id} /></div>
-                            <div className="cart__item-nameprice-container">
-                                <div className="cart__item-name">{product.name}</div>
-                                <div className="cart__item-price">${(product.price / 100).toFixed(2)}</div>
+                        <div className="cart__item--container">
+                            <div className="cart__item">
+                                <div className="cart__item--name">{product.name}</div>
+                                <div className="cart__item--img"><img src={product.imgUrl} alt={product.id} /></div>
                             </div>
-                            <button onClick={handleRemove} id={product.id} className="cart__item-price">Remove</button>
+                            <div className="cart__item--price">${(product.price / 100).toFixed(2)}</div>
+                            <button onClick={handleRemove} id={product.id} className="cart__item--remove">Remove</button>
                         </div>
                     );
                 })}
                 <div>Total: ${(total / 100).toFixed(2)}</div>
                 <button className="cart__checkout--button" onClick={() => props.openModal("checkout")}>Checkout</button>
-            </main>
+            </div>
             <Modal total={total} {...props} />
         </>
     );
