ba84d77106cdc8cb29062a94eb60da53ddce2357
douglasryu <douglasryu@hotmail.com>
2020-07-12 17:35:56 -0400

    Update css

--- src/components/ReviewDiv.js
+++ src/components/ReviewDiv.js
@@ -1,118 +1,118 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { fetchReviews } from "../actions/reviewActions";
 import { baseUrl } from '../config'
 
 const ReviewDiv = (props) => {
-  let reviews = {};
-  let newReviews = [];
-  let [userRev, createRev] = useState('');
+    let reviews = {};
+    let newReviews = [];
+    let [userRev, createRev] = useState('');
 
-  if (props.reviews !== undefined) {
-    reviews = props.reviews;
+    if (props.reviews !== undefined) {
+        reviews = props.reviews;
 
-    Object.values(reviews).forEach((item) => {
-      newReviews.push(item)
-    });
-  }
+        Object.values(reviews).forEach((item) => {
+            newReviews.push(item)
+        });
+    }
 
-  const handleReview = (e) => {
-    createRev(e.target.value)
-  }
+    const handleReview = (e) => {
+        createRev(e.target.value)
+    }
 
-  const handleSubmit = async (e) => {
+    const handleSubmit = async (e) => {
 
-    e.preventDefault()
+        e.preventDefault()
 
-    let userId = props.sessionId
-    let firstName = props.firstName
-    let lastName = props.lastName
-    let productId = props.id
-    let reviewBody = userRev
-
-
-    const res = await fetch(`${baseUrl}/api/reviews/${props.id}`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ userId, firstName, lastName, productId, reviewBody })
-    })
-
-    await props.fetchReviews(props.id);
-    createRev('');
-
-  }
+        let userId = props.sessionId
+        let firstName = props.firstName
+        let lastName = props.lastName
+        let productId = props.id
+        let reviewBody = userRev
 
 
-  if (newReviews.length === 0 && !props.firstName) return null;
+        const res = await fetch(`${baseUrl}/api/reviews/${props.id}`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ userId, firstName, lastName, productId, reviewBody })
+        })
 
-  if (newReviews.length === 0) return (
-    <div className="products__review-container">
-      <form className="products__review-form">
-        <textarea
-          onChange={handleReview}
-          className="products__review-field"
-          type="text"
-          placeholder="Submit a review"
-          value={userRev}
-        ></textarea>
-        <button onClick={handleSubmit} className="products__review-submit">Submit</button>
-      </form>
-    </div>
-  )
+        await props.fetchReviews(props.id);
+        createRev('');
 
-  if (!props.firstName && newReviews.length > 0) return (
-    <div className="products__review-container">
-      {newReviews.map((review, i) => {
-        return (
-          <div key={i} className="products__review-block">
-            <div className="products__review-author">{`${review.firstName} ${review.lastName}`}</div>
-            <div className="products__review-content">{review.reviewBody}</div>
-          </div>
-        );
-      })}
-    </div>
-  )
+    }
 
 
-  return (
-    <div className="products__review-container">
-      {newReviews.map((review, i) => {
-        return (
-          <div key={i} className="products__review-block">
-            <div className="products__review-author">{`${review.firstName} ${review.lastName}`}:</div>
-            <div className="products__review-content">{review.reviewBody}</div>
-          </div>
-        );
-      })}
+    if (newReviews.length === 0 && !props.firstName) return null;
 
-      <form className="products__review-form">
-        <textarea
-          onChange={handleReview}
-          className="products__review-field"
-          type="text"
-          placeholder="Submit a review"
-          value={userRev}
-        ></textarea>
-        <button onClick={handleSubmit} className="products__review-submit">Submit</button>
-      </form>
-    </div>
-  );
+    if (newReviews.length === 0) return (
+        <div className="products__review-container">
+            <form className="products__review-form">
+                <textarea
+                    onChange={handleReview}
+                    className="products__review-field"
+                    type="text"
+                    placeholder="Submit a review"
+                    value={userRev}
+                ></textarea>
+                <button onClick={handleSubmit} className="products__review-submit">Submit</button>
+            </form>
+        </div>
+    )
+
+    if (!props.firstName && newReviews.length > 0) return (
+        <div className="products__review-container">
+            {newReviews.map((review, i) => {
+                return (
+                    <div key={i} className="products__review-block">
+                        <div className="products__review-author">{`${review.firstName} ${review.lastName}`}</div>
+                        <div className="products__review-content">{review.reviewBody}</div>
+                    </div>
+                );
+            })}
+        </div>
+    )
+
+
+    return (
+        <div className="products__review-container">
+            {newReviews.map((review, i) => {
+                return (
+                    <div key={i} className="products__review-block">
+                        <div className="products__review-author">{`${review.firstName} ${review.lastName}`}:</div>
+                        <div className="products__review-content">{review.reviewBody}</div>
+                    </div>
+                );
+            })}
+
+            <form className="products__review-form">
+                <textarea
+                    onChange={handleReview}
+                    className="products__review-field"
+                    type="text"
+                    placeholder="Submit a review"
+                    value={userRev}
+                ></textarea>
+                <button onClick={handleSubmit} className="products__review-submit">Submit</button>
+            </form>
+        </div>
+    );
 };
 
 
 const mapStateToProps = (state) => {
-  return {
-    reviews: state.reviews,
-    sessionId: state.session.id,
-    firstName: state.session.firstName,
-    lastName: state.session.lastName,
-  };
+    return {
+        reviews: state.reviews,
+        sessionId: state.session.id,
+        firstName: state.session.firstName,
+        lastName: state.session.lastName,
+    };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    fetchReviews: (id) => dispatch(fetchReviews(id)),
-  };
+    return {
+        fetchReviews: (id) => dispatch(fetchReviews(id)),
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ReviewDiv);
